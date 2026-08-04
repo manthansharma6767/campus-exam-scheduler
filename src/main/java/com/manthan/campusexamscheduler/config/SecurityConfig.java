fc51e4b8b19e4199d3017ba5ea880b46b0804f3e
Manthan Sharma <manthansharma795@gmail.com>
2026-08-04 23:13:27 +0530

    Configure CORS for React Frontend

--- src/main/java/com/manthan/campusexamscheduler/config/SecurityConfig.java
+++ src/main/java/com/manthan/campusexamscheduler/config/SecurityConfig.java
@@ -53,6 +53,7 @@ public class SecurityConfig {
             throws Exception {
 
         http
+                .cors(cors -> {})
                 .csrf(csrf -> csrf.disable())
 
                 .sessionManagement(session ->
